b72299908a30f00fc32a2cb3a0bdc88939c9090b
Sarina NaserMoghadasi <sarina.nasermoghadasi@email.kntu.ac.ir>
2025-07-05 22:41:21 +0330

    17

--- index.js
+++ index.js
@@ -6,192 +6,174 @@ function getCookie(name) {
 }
 
 function clickLoginButton() {
-    document.getElementById("result").innerHTML = "...درحال تست"
     challenge();
-}
-
-function count_down() {
-    let seconds = 120;
-    const timerElement = document.getElementById("timer");
-
-    const countdown = setInterval(() => {
-      timerElement.textContent = seconds;
-      seconds--;
-
-      if (seconds < 0) {
-        clearInterval(countdown);
-        timerElement.textContent = "Time's up!";
-      }
-    }, 1000);
 }
 
 async function challenge() {
 
     const resultDiv = document.getElementById("result")
-    
-    currentUsername = document.getElementById("username").value;
-    currentPassword = document.getElementById("password").value;
-    currentAuthType = document.getElementById("authTypeDropDown").value;
-    currentType = document.getElementById("typeDropDown").value;
+    send_token()
 
-    userErrorDiv = document.getElementById("userError");
-    passwordErrorDiv = document.getElementById("passwordError");
+    // currentUsername = document.getElementById("username").value;
+    // currentPassword = document.getElementById("password").value;
+    // currentAuthType = document.getElementById("authTypeDropDown").value;
+    // currentType = document.getElementById("typeDropDown").value;
 
-    if (currentUsername.length == 0) {
-        userErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!نام کاربری اجباری است</p>`
-        return;
-    } else {
-        userErrorDiv.innerHTML = ''
-    }
+    // userErrorDiv = document.getElementById("userError");
+    // passwordErrorDiv = document.getElementById("passwordError");
 
-    if (currentPassword.length == 0) {
-        passwordErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!گذرواژه اجباری است</p>`
-        console.log("Entering Password is mandatory.")
-        return;
-    } else if (currentPassword.length < 8) {
-        passwordErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!گذرواژه حداقل 8 کاراکتر مجاز دارد</p>`
-        console.log("Password must atleast have 8 characters.")
-        return;
-    } else {
-        passwordErrorDiv.innerHTML = ''
-    }
+    // if (currentUsername.length == 0) {
+    //     userErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!نام کاربری اجباری است</p>`
+    //     return;
+    // } else {
+    //     userErrorDiv.innerHTML = ''
+    // }
 
-    const dataSending = {
-        username: currentUsername,
-        password: currentPassword,
-        type: currentType,  //"USERPASS", LDAP
-        authType: currentAuthType  //"MOBILE", QR
-    };
+    // if (currentPassword.length == 0) {
+    //     passwordErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!گذرواژه اجباری است</p>`
+    //     console.log("Entering Password is mandatory.")
+    //     return;
+    // } else if (currentPassword.length < 8) {
+    //     passwordErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!گذرواژه حداقل 8 کاراکتر مجاز دارد</p>`
+    //     console.log("Password must atleast have 8 characters.")
+    //     return;
+    // } else {
+    //     passwordErrorDiv.innerHTML = ''
+    // }
 
-    try {
-        const apiURL = "http://172.16.20.173/api/v1/authentication/login/challenge"
+//     const dataSending = {
+//         username: currentUsername,
+//         password: currentPassword,
+//         type: currentType,  //"USERPASS", LDAP
+//         authType: currentAuthType  //"MOBILE", QR
+//     };
+
+//     try {
+//         const apiURL = "http://172.16.20.173/api/v1/authentication/login/challenge"
         
-        const response = await fetch(apiURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(dataSending)
-        })
+//         const response = await fetch(apiURL, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(dataSending)
+//         })
 
-        if (!response.ok) {
-            console.log("response is not ok")
+//         if (!response.ok) {
+//             console.log("response is not ok")
 
-            responseBody = await response.text()
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
-            console.log("Response body:", responseBody); //{"detail":"automatic bind not successful - invalidCredentials"}
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
+//             responseBody = await response.text()
+//             resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
+//             console.log("Response body:", responseBody); //{"detail":"automatic bind not successful - invalidCredentials"}
+//             // const errorMessage = responseBody.errors[0].msg
+//             // console.log(errorMessage)
              
-            //////////////inja bauad dorost she beporsam
+//             //////////////inja bauad dorost she beporsam
 
-            return;
+//             return;
 
-        } else {
-            console.log("response is ok")
-            const data = await response.json();
+//         } else {
+//             console.log("response is ok")
+//             const data = await response.json();
             
-            if (JSON.stringify(data.id)) {
-                console.log("Challenge Successful!!");
+//             if (JSON.stringify(data.id)) {
+//                 console.log("Challenge Successful!!");
 
-                document.cookie = "challengeID=" + JSON.stringify(data.id);
+//                 document.cookie = "challengeID=" + JSON.stringify(data.id);
 
-                if (currentType == "LDAP") {
-                    QR_verification_page();
-                } else if (currentType == "USERPASS") {
-                    SMS_verification_page();
-                }
+//                 if (currentType == "LDAP") {
+//                     QR_verification_page();
+//                 } else if (currentType == "USERPASS") {
+//                     SMS_verification_page();
+//                 }
 
-            } else if (JSON.stringify(data.errors)) {
-                resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error!! ${JSON.stringify(data.errors[0].msg)}</p>`;
-                console.error("Error!!");
-                return;
-            }
-        }
+//             } else if (JSON.stringify(data.errors)) {
+//                 resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error!! ${JSON.stringify(data.errors[0].msg)}</p>`;
+//                 console.error("Error!!");
+//                 return;
+//             }
+//         }
 
-    } catch (error) {
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Network or JavaScript Error! : ${error.message}</p>`;
-        console.error("Error catched: " + errorMessage);
-        return;
-    }
-}
+//     } catch (error) {
+//         resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Network or JavaScript Error! : ${error.message}</p>`;
+//         console.error("Error catched: " + errorMessage);
+//         return;
+//     }
+// }
 
-function QR_verification_page() {
-    const resultDiv = document.getElementById("result");
-    if (resultDiv){
-        resultDiv.innerHTML = "...درحال بررسی"
-    }
-    window.location.assign("QRAuthenticationPage.html");
-}
+// function QR_verification_page() {
+//     const resultDiv = document.getElementById("result");
+//     window.location.assign("QRAuthenticationPage.html");
+// }
 
-async function handle_QR_authentication() {
+// async function handle_QR_authentication() {
 
-    const resultDiv = document.getElementById("result")
+//     const resultDiv = document.getElementById("result")
 
-    try {
-        const authenticationCode = document.getElementById("authenticationCode").value;
+//     try {
+//         const authenticationCode = document.getElementById("authenticationCode").value;
 
-        if (authenticationCode.length == 0) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!وارد کردن کد اجباری است</p>`;
-            return;
-        } else if (authenticationCode.length < 6) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!کد باید 4 رقمی باشد</p>`;
-            return;
-        }
+//         if (authenticationCode.length == 0) {
+//             resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!وارد کردن کد اجباری است</p>`;
+//             return;
+//         } else if (authenticationCode.length < 6) {
+//             resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!کد باید 4 رقمی باشد</p>`;
+//             return;
+//         }
 
-        const rawChallengeID = getCookie("challengeID");
-        const challengeID = rawChallengeID.replace(/^"|"$/g, '');
+//         const rawChallengeID = getCookie("challengeID");
+//         const challengeID = rawChallengeID.replace(/^"|"$/g, '');
 
-        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/totp/verify`;
+//         const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/totp/verify`;
 
-        const response = await fetch(apiURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                code: authenticationCode
-            })
-        })
+//         const response = await fetch(apiURL, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify({
+//                 code: authenticationCode
+//             })
+//         })
 
-        if (!response.ok) {
-            console.log("response not ok.")
+//         if (!response.ok) {
+//             console.log("response not ok.")
 
-            responseBody = await response.text()
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
-            console.log("Response body:", responseBody);
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
+//             responseBody = await response.text()
+//             resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
+//             console.log("Response body:", responseBody);
+//             // const errorMessage = responseBody.errors[0].msg
+//             // console.log(errorMessage)
              
-            //////////////inja bauad dorost she beporsam
+//             //////////////inja bauad dorost she beporsam
 
-        } else {
-            console.log("response is ok.")
+//         } else {
+//             console.log("response is ok.")
 
-            const data = await response.json();
+//             const data = await response.json();
 
-            resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Successful! ${JSON.stringify(data)}}</p>`;
-            console.log("Successful: " + JSON.stringify(data));
+//             resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Successful! ${JSON.stringify(data)}}</p>`;
+//             console.log("Successful: " + JSON.stringify(data));
 
-            if (data.message === "Successful") {
+//             if (data.message === "Successful") {
 
-                resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Authentication successful!</p>`
-                console.log("QR Authentication Successful.")
+//                 resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Authentication successful!</p>`
+//                 console.log("QR Authentication Successful.")
 
-                successful_login_page();
+//                 successful_login_page();
                 
-            } else {
-                resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Authentication failed. Try again.</p>`
-                console.log("QR Authentication unsuccessful.")
-            }
-        }
-    }
+//             } else {
+//                 resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Authentication failed. Try again.</p>`
+//                 console.log("QR Authentication unsuccessful.")
+//             }
+//         }
+//     }
 
-    catch(error) {
-        const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error catched: ${error.message}</p>`
-        console.log("Error catched: " + error.message)
-    }
+//     catch(error) {
+//         const resultDiv = document.getElementById("result");
+//         resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error catched: ${error.message}</p>`
+//         console.log("Error catched: " + error.message)
+//     }
 }
 
 function SMS_verification_page() {
@@ -251,7 +233,6 @@ async function send_sms_to_mobile() {
         document.cookie = "mobile=" + data.mobile;
         document.cookie = "expires_in=" + data.expires_in;
 
-        // Showing phone number
         let resultDiv2 = document.getElementById("result2");
         resultDiv2.innerHTML = data.mobile
         console.log(data.mobile)
@@ -366,7 +347,7 @@ async function get_access_token() {
         if (data.message === "Successful") {
             resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅"Access Token Stored, token: ${data.access_token}"</p>`
             console.log("Access Token Stored, token: " + data.access_token)
-            // what to do next?               
+            document.cookie = "access_token=" + data.access_token               
         } else {
             resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">Operation failed. Try again.</p>`
             console.log("Unsuccessful.")
@@ -380,6 +361,30 @@ async function get_access_token() {
     }
 }
 
-function give_access_to_data() {
-    // now the user has access to the data
+async function send_token() {
+    // const accessToken = getCookie(access_token)
+    const resultDiv = document.getElementById("result")
+
+    try {
+        let apiURL = "http://localhost:3000/addToken"
+        const response = await fetch (apiURL , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ 
+                value: "salam"
+            })
+        })
+        console.log("Request for updating token was sent successfully.")
+
+        const data = await response.text();
+        console.log(data)
+        resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">${data}</p>`
+
+    } catch (error) {
+        let resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the token to DB: ${error.message}</p>`
+        console.log("Catched error: " + error.message)
+    }    
 }
---
+++ server/server.js
@@ -0,0 +1,48 @@
+// INSERT INTO tokenTable VALUES (@value)
+// UPDATE tokenTable SET token = @Value
+
+import express from "express"
+import mssql from "mssql/msnodesqlv8.js"
+import cors from "cors"
+import { DateTime } from "msnodesqlv8"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// const sqlServerConfiguration = {
+//     // user: "DESKTOP-GBBCHEM/sarina",
+//     // password: "",
+//     server: "DESKTOP-GBBCHEM/sarina",
+//     database: "tokenDB",
+//     driver: 'msnodesqlv8',
+//     options: {
+//         trustedConnection: true
+//     }
+// }
+
+const sqlServerConfiguration = {
+    connectionString: "Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-GBBCHEM;Database=tokenDB;Trusted_Connection=Yes;"
+}
+
+app.post("/addToken", async (req, res) => {
+    try {
+        const pool = await mssql.connect(sqlServerConfiguration)
+        await pool.request()
+            .input("value", mssql.VarChar, req.body.value)
+            .query("UPDATE tokenTable SET token = @Value")
+
+        const now = new Date()
+        console.log("Token updated successfully at: " + now.toLocaleString())
+        res.status(200).send("Token updated successfully at: " + now.toLocaleString())
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+})
+
+const port = 3000
+app.listen(port , () => {
+    console.log("Server running on port " + port + ".")
+})
