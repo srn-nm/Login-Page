16fc598f031c32a8eed559321b137a9ecec33095
NaserMoghadasi Sarina <S.NaserMoghadasi@kayson-ir.com>
2025-07-07 18:01:15 +0330

    19

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+let globalAccessToken;
+let globalChallengeID;
+
+function modifyAccessToken(newTokenAccess) {
+    globalAccessToken = newTokenAccess;
 }
 
 async function challenge() {
@@ -16,7 +16,7 @@ async function challenge() {
 
     userErrorDiv = document.getElementById("userError");
     passwordErrorDiv = document.getElementById("passwordError");
-
+    
     if (currentUsername.length == 0) {
         userErrorDiv.innerHTML = `<p style="color: red; font-size: 14px;">!نام کاربری اجباری است</p>`
         return;
@@ -44,9 +44,8 @@ async function challenge() {
     };
 
     try {
-        const apiURL = "http://172.16.20.173/api/v1/authentication/login/challenge"
-        
-        const response = await fetch(apiURL, {
+        let apiURL = "http://localhost:3000/challenge"
+        const response = await fetch (apiURL , {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -54,187 +53,119 @@ async function challenge() {
             body: JSON.stringify(dataSending)
         })
 
-        if (!response.ok) {
-            console.log("response is not ok")
+        console.log("Request to localHost Challenge was sent successfully.")
 
-            responseBody = await response.text()
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
-            console.log("Response body:", responseBody); //{"detail":"automatic bind not successful - invalidCredentials"}
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
-            //////////////inja bauad dorost she beporsam
+        if (response.status == 200) {
+            const data = await response.text();
 
-            return;
+            console.log("New Challenge ID: " + data)
 
-        } else {
-            console.log("response is ok")
-            const data = await response.json();
+            let globalChallengeID = data;
+
+            if (currentType == "LDAP") {
+                window.location.assign("QRAuthenticationPage.html");
+                const resultDiv = document.getElementById("result");
             
-            if (JSON.stringify(data.id)) {
-                console.log("Challenge Successful!!");
-
-                document.cookie = "challengeID=" + JSON.stringify(data.id);
-
-                if (currentType == "LDAP") {
-                    QR_verification_page();
-                } else if (currentType == "USERPASS") {
-                    SMS_verification_page();
-                }
-
-            } else if (JSON.stringify(data.errors)) {
-                resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error!! ${JSON.stringify(data.errors[0].msg)}</p>`;
-                console.error("Error!!");
-                return;
+            } else if (currentType == "USERPASS") {
+                const resultDiv = document.getElementById("result");
+                send_sms_to_mobile();
+                window.location.assign("SMSVerificationPage.html");
             }
+        } else {
+            const error = await response.text();
+            console.log("Error " + response.status + " " + error )
         }
-
+        
     } catch (error) {
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Network or JavaScript Error! : ${error.message}</p>`;
-        console.error("Error catched: " + errorMessage);
-        return;
-    }
+        const resultDiv = document.getElementById("result");
+        if (resultDiv) {
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the request to localHost: ${error.message}</p>`
+        }
+        console.log("Catched error: " + error)
+    }  
 }
 
-function QR_verification_page() {
-    const resultDiv = document.getElementById("result");
-    window.location.assign("QRAuthenticationPage.html");
-}
 
 async function handle_QR_authentication() {
 
     const resultDiv = document.getElementById("result")
+    const authenticationCode = document.getElementById("authenticationCode").value;
 
-    try {
-        const authenticationCode = document.getElementById("authenticationCode").value;
-
-        if (authenticationCode.length == 0) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!وارد کردن کد اجباری است</p>`;
-            return;
-        } else if (authenticationCode.length < 6) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!کد باید 4 رقمی باشد</p>`;
-            return;
-        }
-
-        const rawChallengeID = getCookie("challengeID");
-        const challengeID = rawChallengeID.replace(/^"|"$/g, '');
-
-        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/totp/verify`;
-
-        const response = await fetch(apiURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                code: authenticationCode
-            })
-        })
-
-        if (!response.ok) {
-            console.log("response not ok.")
-
-            responseBody = await response.text()
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
-            console.log("Response body:", responseBody);
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
-             
-            //////////////inja bauad dorost she beporsam
-
-        } else {
-            console.log("response is ok.")
-
-            const data = await response.json();
-
-            resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Successful! ${JSON.stringify(data)}}</p>`;
-            console.log("Successful: " + JSON.stringify(data));
-
-            if (data.message === "Successful") {
-
-                resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Authentication successful!</p>`
-                console.log("QR Authentication Successful.")
-
-                successful_login_page();
-                
-            } else {
-                resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Authentication failed. Try again.</p>`
-                console.log("QR Authentication unsuccessful.")
-            }
-        }
-    }
-
-    catch(error) {
-        const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error catched: ${error.message}</p>`
-        console.log("Error catched: " + error.message)
-    }
-}
-
-function SMS_verification_page() {
-    let resultDiv = document.getElementById("result");
-
-    window.location.assign("SMSVerificationPage.html")
-
-    send_sms_to_mobile();
-
-    count_down();
-
-    let resultDiv2 = document.getElementById("result2");
-    if (resultDiv2){
-        resultDiv2.innerHTML = getCookie("mobile"); 
-    }
-}
-
-async function send_sms_to_mobile() {
-    
-    const rawChallengeID = getCookie("challengeID");
-    const challengeID = rawChallengeID.replace(/^"|"$/g, '');
-    
-    if (!challengeID) {
-        console.error("No challengeID found!");
+    if (authenticationCode.length == 0) {
+        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!وارد کردن کد اجباری است</p>`;
+        return;
+    } else if (authenticationCode.length < 6) {
+        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">!کد باید 4 رقمی باشد</p>`;
         return;
     }
-    
-    let resultDiv = document.getElementById("result");
-
-    console.log("Challenge ID: " + challengeID)
 
     try {
-        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile`;
-        
-        console.log(apiURL)
-
-        const response = await fetch(apiURL, {
+        let apiURL = "http://localhost:3000/handle_QR_authentication"
+        const response = await fetch (apiURL , {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "id": challengeID
+                authenticationCode: authenticationCode,
+                challengeID: globalChallengeID
             })
         })
 
-        const data = await response.json();
+        console.log("Request to localHost handle_QR_authentication was sent successfully.")
 
-        if (!response.ok) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;"> response is not ok. ${response}</p>`;
-            console.log("Sending sms to mobile's Response is not OK.")
+        if (response.status == 200) {
+            console.log("QR Authentication Successful.")
+            successful_login_page();
+            
+        } else {
+            console.log("QR Authentication unsuccessful.")
+            const error = await response.text();
+            console.log("Error " + response.status + " " + error )
+            const resultDiv = document.getElementById("result")
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;"> ${error}</p>`;
+            return;
         }
-
-        resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Successful! ${JSON.stringify(data)}}</p>`;
-        console.log("Sending SMS to mobile was successful!")
-
-        document.cookie = "mobile=" + data.mobile;
-        document.cookie = "expires_in=" + data.expires_in;
-
-        let resultDiv2 = document.getElementById("result2");
-        resultDiv2.innerHTML = data.mobile
-        console.log(data.mobile)
+    } catch (error) {
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the request to localHost: ${error.message}</p>`
+        console.log("Catched error: " + error)
     }
+}
 
-    catch(error) {
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error catched. Reason: (${(error.Message)})</p>`;
-        console.log("Sending SMS to mobile: Error catched. " + error)
+async function send_sms_to_mobile() {   
+    const resultDiv = document.getElementById("result");
+
+    try {
+        let apiURL = "http://localhost:3000/send_sms_to_mobile"
+        const response = await fetch (apiURL , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                challengeID: globalChallengeID
+            })
+        })
+
+        console.log("Request to localHost send_sms_to_mobile was sent successfully.")
+
+        if (response.status == 200) {
+            console.log("SMS sent successfully.")
+            
+        } else {
+            console.log("SMS wasnt sent successfully.")
+            const error = await response.text();
+            console.log("Error " + response.status + " " + error )
+            return;
+        }
+        
+    } catch (error) {
+        const resultDiv = document.getElementById("result");
+        if (resultDiv) {
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the request to localHost: ${error.message}</p>`
+        }
+        console.log("Catched error: " + error)
     }
 }
 
@@ -251,80 +182,54 @@ async function handle_SMS_verification() {
         return;
     }
 
-    const rawChallengeID = getCookie("challengeID");
-    const challengeID = rawChallengeID.replace(/^"|"$/g, '');
-    
-    if (!challengeID) {
-        console.error("No challenge ID found!");
-        return;
-    }
-
     try {
-        const resultDiv = document.getElementById("result");
-        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile/verify`;
-
-        const response = await fetch(apiURL, {
+        let apiURL = "http://localhost:3000/handle_SMS_verification"
+        const response = await fetch (apiURL , {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                code: verificationCode
+                verificationCode: verificationCode,
+                challengeID: globalChallengeID
             })
         })
 
-        if (!response.ok) {
+        console.log("Request to localHost handle_SMS_verification was sent successfully.")
 
-            responseBody = await response.text()
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ ${responseBody}</p>`
-            console.log("Response body:", responseBody);  //
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
-             
-            //////////////inja bauad dorost she beporsam
-
+        if (response.status == 200) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Verification successful!</p>`
+            console.log("Verification Successful.")
+            successful_login_page();
+            
         } else {
-            const data = await response.json();
-
-            if (resultDiv) {
-                if (data.message === "Successful") {
-                    resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Verification successful!</p>`
-                    console.log("Verification Successful.")
-                    this.successful_login_page();
-                    
-                } else {
-                    resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Verification failed. Try again.</p>`
-                    console.error("Verification failed.")
-                    return;
-                }
-            } else {
-                console.error("Error: 'result' div not found!");
-                return;
-            }
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Verification failed. Try again.</p>`
+            console.error("Verification failed.")
+            const error = await response.text();
+            console.log("Error " + response.status + " " + error )
+            return;
         }
-    }
-
-    catch(error) {
-        let resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error verifying code. ${error.message}</p>`
-        console.log("Catched error in sms verification: " + error.message)
+        
+    } catch (error) {
+        const resultDiv = document.getElementById("result");
+        if (resultDiv) {
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the request to localHost: ${error.message}</p>`
+        }
+        console.log("Catched error: " + error)
     }
 }
 
 function successful_login_page() {
-    window.location.assign("pages/successfulLoginPage.html");
+    window.location.assign("successfulLoginPage.html");
     get_access_token();
 }
 
 async function get_access_token() {
-
     try {
-        const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `<p style="color: blue; font-size: 18px;">...در حال بررسی دسترسی</p>`
-
-        const apiURL = `http://172.16.20.173/api/v1/authentication/login/access-token`;
-
-        const response = await fetch(apiURL, {
+        let apiURL = "http://localhost:3000/get_access_token"
+        const response = await fetch (apiURL , {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -332,32 +237,35 @@ async function get_access_token() {
             body: JSON.stringify({})
         })
 
-        const data = await response.json();
+        console.log("Request to localHost get_access_token was sent successfully.")
 
-        if (!response.ok) {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;"> response is not ok. ${response.status}</p>`;
+        if (response.status == 200) {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅ Acccess token updated.</p>`
+            console.log("Acccess token updated.")
+            modifyAccessToken(response.text())
+    
+            // what to do next
+        } else {
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Getting access token failed. Try again.</p>`
+            console.error("Getting access token failed.")
+            const error = await response.text();
+            console.log("Error " + response.status + " " + error )
+            return;
         }
         
-        if (data.message === "Successful") {
-            resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">✅"Access Token Stored, token: ${data.access_token}"</p>`
-            console.log("Access Token Stored, token: " + data.access_token)
-            document.cookie = "access_token=" + data.access_token               
-        } else {
-            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">Operation failed. Try again.</p>`
-            console.log("Unsuccessful.")
-        }
-    }
-
-    catch(error) {
+    } catch (error) {
         const resultDiv = document.getElementById("result");
-        resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌Error storing token.</p>`
-        console.log("Error catched: " + error.message)
+        if (resultDiv) {
+            resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the request to localHost: ${error.message}</p>`
+        }
+        console.log("Catched error: " + error)
     }
 }
 
 async function send_token() {
-    const accessToken = getCookie(access_token)
-    const resultDiv = document.getElementById("result")
+    accessToken = globalAccessToken
 
     try {
         let apiURL = "http://localhost:3000/addToken"
@@ -366,19 +274,20 @@ async function send_token() {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ 
-                value: accessToken
+            body: JSON.stringify({
+                accessToken: accessToken
             })
         })
         console.log("Request for updating token was sent successfully.")
 
         const data = await response.text();
         console.log(data)
+        const resultDiv = document.getElementById("result");
         resultDiv.innerHTML = `<p style="color: green; font-size: 18px;">${data}</p>`
 
     } catch (error) {
-        let resultDiv = document.getElementById("result");
+        const resultDiv = document.getElementById("result");
         resultDiv.innerHTML = `<p style="color: red; font-size: 18px;">❌ Error in sending the token to DB: ${error.message}</p>`
-        console.log("Catched error: " + error.message)
+        console.log("Catched error: " + error)
     }    
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 // INSERT INTO tokenTable VALUES (@value)
 // UPDATE tokenTable SET token = @Value
 
+
 import express from "express"
 import mssql from "mssql/msnodesqlv8.js"
 import cors from "cors"
 
+
 const app = express()
 app.use(express.json())
 app.use(cors())
+
 
 // const sqlServerConfiguration = {
 //     // user: "DESKTOP-GBBCHEM/sarina",
@@ -24,11 +27,221 @@ const sqlServerConfiguration = {
     connectionString: "Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-GBBCHEM;Database=tokenDB;Trusted_Connection=Yes;"
 }
 
+app.post("/challenge", async (req, res) => {
+    try {
+        const dataSending = {
+            username: req.body.username,
+            password: req.body.password,
+            type: req.body.type,  
+            authType: req.body.authType  
+        }
+        const apiURL = "http://172.16.20.173/api/v1/authentication/login/challenge"
+        
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dataSending)
+        })
+
+        if (!response.ok) {
+            console.log("response is not ok")
+
+            let responseBody = await response.text()
+            console.log("Response body:", responseBody); 
+            
+            res.status(500).send("Response not ok: " + responseBody)
+
+            // const errorMessage = responseBody.errors[0].msg
+            // console.log(errorMessage)
+            //////////////inja bauad dorost she beporsam
+            return;
+
+        } else {
+            console.log("response is ok")
+            const data = await response.json();
+           
+            console.log("Challenge Successful!!");
+
+            res.status(200).send(data.id)
+        }
+
+    } catch (error) {
+        console.error("Error catched: " + error);
+        res.status(500).send("Error catched: " + error)
+        return;
+    }
+})
+
+app.post("/handle_QR_authentication", async (req, res) => {
+    try {
+        const authenticationCode = req.body.authenticationCode
+        // let rawChallengeID = req.body.challengeID 
+        // let challengeID = rawChallengeID.replace(/^"|"$/g, '');
+        
+        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/totp/verify`;
+
+        console.log(apiURL)
+        console.log(req.body.challengeID)
+        console.log(authenticationCode)
+
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                code: authenticationCode
+            })
+        })
+
+        if (!response.ok) {
+            console.log("Handle QR authentication's response is not ok.")
+
+            let responseBody = await response.text()
+            console.log("Response body:", responseBody);
+
+            res.status(response.status).send("ResponseBody: " + responseBody)
+
+            // const errorMessage = responseBody.errors[0].msg
+            // console.log(errorMessage)
+            //////////////inja bauad dorost she beporsam
+
+            return;
+
+        } else {
+            console.log("response is ok.")
+            const data = await response.json();
+            console.log("Successful: " + JSON.stringify(data));
+            res.status(200).send("Successful")
+        }
+    } catch(error) {
+        console.log("Error catched: " + error.message)
+        return
+    }
+})
+
+app.post("/send_sms_to_mobile", async (req, res) => {
+        let challengeID = req.body.challengeID //.replace(/^"|"$/g, '');
+    try {
+        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile`;
+
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "id": challengeID
+            })
+        })
+
+        if (!response.ok) {
+            console.log("Sending sms to mobile's Response is not OK.")
+            let responseBody = await response.text()
+            res.status(500).send("Response not ok: " + responseBody)
+
+        } else {
+            console.log("Sending SMS to mobile was successful!")
+            res.status(200).send("Sending SMS to mobile was successful!")
+        }
+    } catch(error) {
+        console.log("Sending SMS to mobile: Error catched. " + error)
+        res.status(500).send("Sending SMS to mobile: Error catched. " + error)
+    }
+})
+
+
+app.post("/handle_SMS_verification", async (req, res) => {
+    const challengeID = req.body.challengeID //.replace(/^"|"$/g, '');
+    const verificationCode = req.body.verificationCode
+    
+    if (!challengeID) {
+        console.error("No challenge ID found!");
+        return;
+    }
+
+    try {
+        const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile/verify`;
+
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                code: verificationCode
+            })
+        })
+
+        if (!response.ok) {
+            let responseBody = await response.text()
+            console.log("Response body:", responseBody);  //
+            // const errorMessage = responseBody.errors[0].msg
+            // console.log(errorMessage)
+             
+            //////////////inja bauad dorost she 
+            res.status(500).send(responseBody)
+
+        } else {
+            const data = await response.json();
+            res.status(200).send("Successful")
+        }
+    } catch(error) {
+        console.log("Catched error in sms verification: " + error.message)
+        res.status(500).send("Error in SMS Verification: Error catched. " + error)
+    }
+})
+
+app.post("/get_access_token", async (req, res) => {
+    try {
+        const apiURL = `http://172.16.20.173/api/v1/authentication/login/access-token`;
+
+        const response = await fetch(apiURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({})
+        })
+
+        if (!response.ok) {
+            console.log("Response failed in get access token"); 
+            let responseBody = await response.text()
+            console.log("Response body:", responseBody);
+            res.status(500).send(responseBody)
+            return;
+        } else {   
+            const data = await response.json();
+            if (!data.access_token) {
+                console.log("access token doesnt exist.")
+                res.status(500).send("access token doesnt exist.")
+                return;
+            }
+
+            if (data.message === "Successful") {
+                console.log("Access Token Stored, token: " + data.access_token)
+                res.status(200).send(data.access_token)             
+            } else {
+                console.log("Unsuccessful.")
+                res.status(500).send("Unsuccessful.")
+                return;
+            }            
+        }
+    } catch(error) {
+        console.log("Error catched in getting access token: " + error.message)
+        res.status(500).send(error)
+    }
+})
+
 app.post("/addToken", async (req, res) => {
+    const accessToken = req.body.accessToken
+    
     try {
         const pool = await mssql.connect(sqlServerConfiguration)
         await pool.request()
-            .input("value", mssql.VarChar, req.body.value)
+            .input("value", mssql.VarChar, accessToken)
             .query("UPDATE tokenTable SET token = @Value")
 
         const now = new Date()
