13c9d9a4beaef09bd131b51b40aa0c74d824ec4c
NaserMoghadasi Sarina <S.NaserMoghadasi@kayson-ir.com>
2025-07-12 18:28:44 +0330

    20

--- server/server.js
+++ server/server.js
@@ -1,31 +1,13 @@
-// INSERT INTO tokenTable VALUES (@value)
-// UPDATE tokenTable SET token = @Value
-
-
+import fs from "fs"
 import express from "express"
-import mssql from "mssql/msnodesqlv8.js"
 import cors from "cors"
-
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-
-// const sqlServerConfiguration = {
-//     // user: "DESKTOP-GBBCHEM/sarina",
-//     // password: "",
-//     server: "DESKTOP-GBBCHEM/sarina",
-//     database: "tokenDB",
-//     driver: 'msnodesqlv8',
-//     options: {
-//         trustedConnection: true
-//     }
-// }
-
-const sqlServerConfiguration = {
-    connectionString: "Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-GBBCHEM;Database=tokenDB;Trusted_Connection=Yes;"
-}
 
 app.post("/challenge", async (req, res) => {
     try {
@@ -35,6 +17,7 @@ app.post("/challenge", async (req, res) => {
             type: req.body.type,  
             authType: req.body.authType  
         }
+
         const apiURL = "http://172.16.20.173/api/v1/authentication/login/challenge"
         
         const response = await fetch(apiURL, {
@@ -47,44 +30,40 @@ app.post("/challenge", async (req, res) => {
 
         if (!response.ok) {
             console.log("response is not ok")
-
-            let responseBody = await response.text()
-            console.log("Response body:", responseBody); 
             
-            res.status(500).send("Response not ok: " + responseBody)
+            let responseBody = await response.text()
+            console.log("Response body:", responseBody);
+            res.status(response.status).send(responseBody)
 
-            // const errorMessage = responseBody.errors[0].msg
-            // console.log(errorMessage)
-            //////////////inja bauad dorost she beporsam
+            // const responseData = await response.json();
+            // console.log(responseData.errors[0].msg);
+            // res.status(response.status).send(responseData.errors[0].msg)
+
+            // responseBody = {"errors":[{"msg":"Invalid credentials","code":1}]}
             return;
-
         } else {
             console.log("response is ok")
             const data = await response.json();
            
-            console.log("Challenge Successful!!");
-
+            console.log("Challenge Successful.");
             res.status(200).send(data.id)
         }
-
     } catch (error) {
         console.error("Error catched: " + error);
-        res.status(500).send("Error catched: " + error)
+        res.status(response.status).send("Error catched: " + error)
         return;
     }
 })
 
 app.post("/handle_QR_authentication", async (req, res) => {
     try {
+        const challengeID = req.body.challengeID
         const authenticationCode = req.body.authenticationCode
         // let rawChallengeID = req.body.challengeID 
         // let challengeID = rawChallengeID.replace(/^"|"$/g, '');
         
         const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/totp/verify`;
-
-        console.log(apiURL)
-        console.log(req.body.challengeID)
-        console.log(authenticationCode)
+        // console.log(apiURL)
 
         const response = await fetch(apiURL, {
             method: "POST",
@@ -102,12 +81,12 @@ app.post("/handle_QR_authentication", async (req, res) => {
             let responseBody = await response.text()
             console.log("Response body:", responseBody);
 
-            res.status(response.status).send("ResponseBody: " + responseBody)
+            res.status(response.status).send(responseBody)
 
+            // {"errors":[{"msg":"Invalid credentials","code":1}]}
             // const errorMessage = responseBody.errors[0].msg
             // console.log(errorMessage)
             //////////////inja bauad dorost she beporsam
-
             return;
 
         } else {
@@ -117,13 +96,14 @@ app.post("/handle_QR_authentication", async (req, res) => {
             res.status(200).send("Successful")
         }
     } catch(error) {
-        console.log("Error catched: " + error.message)
+        console.log("Handle QR Authentication: Error catched: " + error.message)
+        res.status(500).send("Handle QR Authentication: Error catched. " + error)
         return
     }
 })
 
 app.post("/send_sms_to_mobile", async (req, res) => {
-        let challengeID = req.body.challengeID //.replace(/^"|"$/g, '');
+        let challengeID = req.body.challengeID 
     try {
         const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile`;
 
@@ -133,14 +113,16 @@ app.post("/send_sms_to_mobile", async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "id": challengeID
+                id: challengeID
             })
         })
 
+        console.log(apiURL)
+
         if (!response.ok) {
-            console.log("Sending sms to mobile's Response is not OK.")
             let responseBody = await response.text()
-            res.status(500).send("Response not ok: " + responseBody)
+            console.log("Sending sms to mobile's Response is not OK. Response body:" + responseBody)
+            res.status(response.status).send(responseBody)
 
         } else {
             console.log("Sending SMS to mobile was successful!")
@@ -152,16 +134,14 @@ app.post("/send_sms_to_mobile", async (req, res) => {
     }
 })
 
-
 app.post("/handle_SMS_verification", async (req, res) => {
-    const challengeID = req.body.challengeID //.replace(/^"|"$/g, '');
+    const challengeID = req.body.challengeID 
     const verificationCode = req.body.verificationCode
     
     if (!challengeID) {
         console.error("No challenge ID found!");
         return;
     }
-
     try {
         const apiURL = `http://172.16.20.173/api/v1/authentication/login/challenge/${challengeID}/mobile/verify`;
 
@@ -177,19 +157,19 @@ app.post("/handle_SMS_verification", async (req, res) => {
 
         if (!response.ok) {
             let responseBody = await response.text()
-            console.log("Response body:", responseBody);  //
+            console.log(responseBody);  //
             // const errorMessage = responseBody.errors[0].msg
             // console.log(errorMessage)
              
             //////////////inja bauad dorost she 
-            res.status(500).send(responseBody)
+            res.status(response.status).send(responseBody)
 
         } else {
             const data = await response.json();
             res.status(200).send("Successful")
         }
     } catch(error) {
-        console.log("Catched error in sms verification: " + error.message)
+        console.log("Catched error in SMS verification: " + error.message)
         res.status(500).send("Error in SMS Verification: Error catched. " + error)
     }
 })
@@ -201,22 +181,23 @@ app.post("/get_access_token", async (req, res) => {
         const response = await fetch(apiURL, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Credentials": "include"
             },
             body: JSON.stringify({})
         })
 
         if (!response.ok) {
-            console.log("Response failed in get access token"); 
+            console.log("Response failed in getting access token."); 
             let responseBody = await response.text()
-            console.log("Response body:", responseBody);
-            res.status(500).send(responseBody)
+            console.log(responseBody);
+            res.status(response.status).send(responseBody)
             return;
         } else {   
             const data = await response.json();
             if (!data.access_token) {
-                console.log("access token doesnt exist.")
-                res.status(500).send("access token doesnt exist.")
+                console.log("Access token doesnt exist.")
+                res.status(response.status).send("Access token doesnt exist.")
                 return;
             }
 
@@ -225,7 +206,7 @@ app.post("/get_access_token", async (req, res) => {
                 res.status(200).send(data.access_token)             
             } else {
                 console.log("Unsuccessful.")
-                res.status(500).send("Unsuccessful.")
+                res.status(response.status).send("Unsuccessful.")
                 return;
             }            
         }
@@ -235,26 +216,20 @@ app.post("/get_access_token", async (req, res) => {
     }
 })
 
-app.post("/addToken", async (req, res) => {
-    const accessToken = req.body.accessToken
-    
-    try {
-        const pool = await mssql.connect(sqlServerConfiguration)
-        await pool.request()
-            .input("value", mssql.VarChar, accessToken)
-            .query("UPDATE tokenTable SET token = @Value")
+app.post("/addTokenToDB", async (req, res) => {
+    const newAccessToken = req.body.newAccessToken
 
-        const now = new Date()
-        console.log("Token updated successfully at: " + now.toLocaleString())
-        res.status(200).send("Token updated successfully at: " + now.toLocaleString())
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const csvPath = path.join(__dirname, "data.csv");
 
-    } catch (error) {
-        console.error(error)
-        res.status(500).send(error)
-    }
+    const newValue = newAccessToken;
+    fs.writeFileSync(csvPath, newValue, "utf-8");
+
+    res.status(200).send("Successfull.")
 })
 
-const port = 3000
+const port = 4000
 app.listen(port , () => {
     console.log("Server running on port " + port + ".")
 })
